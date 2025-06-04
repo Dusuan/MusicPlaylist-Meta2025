289345ff92d8d07875c2fec151f0da7274c5fc36
Juan Manuel Sandoval Ramírez <dusuan@meta.com>
2025-06-04 11:00:57 -0700

    Implemented opening and closing modal with Javascript

--- music-playlist-creator/script.js
+++ music-playlist-creator/script.js
@@ -1,1 +1,30 @@
+const modal = document.getElementById("modal")
+const card = document.getElementById("card")
+const closeModal = document.getElementById("close")
 
+card.addEventListener("click", setModalOpen);
+closeModal.addEventListener("click", setModalClosed);
+
+
+function setModalClosed(){
+    modal.style.display = "none"
+}
+function setModalOpen(){
+    modal.style.display = "block"
+}
+
+
+
+
+
+function sumOfEvens(int){
+    let sum = 0;
+    for(let i = 0; i<= number; i++){
+        if(i % 2 === 0){
+            sum+=i;
+        }
+    }
+    return sum;
+}
+
+console.log(sumOfEvens(sum))
