0ae7c6d2567bb4f34e8740779bab1255282a66c3
Juan Manuel Sandoval Ramírez <dusuan@meta.com>
2025-06-06 00:17:31 -0700

    some styling

--- music-playlist-creator/script.js
+++ music-playlist-creator/script.js
@@ -48,7 +48,7 @@ async function renderPlaylist(Playlists) {
             <p>${element.playlist_author}</p>
             </div>
             <div class="deleteCard" style="margin-right: 10px">
-              <img class="" src="" width="10px" height="10px" />
+              x
             </div>
           </div>
 
@@ -56,7 +56,7 @@ async function renderPlaylist(Playlists) {
           <div
             class="cardMenu"
           >
-            <img class="likeBtn" src="" width="10px" height="10px" />
+            <svg class="likeBtn" xmlns="http://www.w3.org/2000/svg" height="20" width="20" viewBox="0 0 512 512"><!--!Font Awesome Free 6.7.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2025 Fonticons, Inc.--><path fill="#ffffff" d="M225.8 468.2l-2.5-2.3L48.1 303.2C17.4 274.7 0 234.7 0 192.8l0-3.3c0-70.4 50-130.8 119.2-144C158.6 37.9 198.9 47 231 69.6c9 6.4 17.4 13.8 25 22.3c4.2-4.8 8.7-9.2 13.5-13.3c3.7-3.2 7.5-6.2 11.5-9c0 0 0 0 0 0C313.1 47 353.4 37.9 392.8 45.4C462 58.6 512 119.1 512 189.5l0 3.3c0 41.9-17.4 81.9-48.1 110.4L288.7 465.9l-2.5 2.3c-8.2 7.6-19 11.9-30.2 11.9s-22-4.2-30.2-11.9zM239.1 145c-.4-.3-.7-.7-1-1.1l-17.8-20-.1-.1s0 0 0 0c-23.1-25.9-58-37.7-92-31.2C81.6 101.5 48 142.1 48 189.5l0 3.3c0 28.5 11.9 55.8 32.8 75.2L256 430.7 431.2 268c20.9-19.4 32.8-46.7 32.8-75.2l0-3.3c0-47.3-33.6-88-80.1-96.9c-34-6.5-69 5.4-92 31.2c0 0 0 0-.1 .1s0 0-.1 .1l-17.8 20c-.3 .4-.7 .7-1 1.1c-4.5 4.5-10.6 7-16.9 7s-12.4-2.5-16.9-7z"/></svg>
             <p> ${element.likeCount} Likes</p>
           </div>
         </div>
@@ -98,7 +98,7 @@ function deleteCard(Playlists, card) {
 function openModal(PlaylistData) {
   xbtn = document.createElement("div");
   xbtn.innerHTML = `<div id="close" class="x" style="width: 20px; height: 20px; padding: 10px">
-            X
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512"><!--!Font Awesome Free 6.7.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2025 Fonticons, Inc.--><path fill="#ffffff" d="M342.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L192 210.7 86.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L146.7 256 41.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 301.3 297.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L237.3 256 342.6 150.6z"/></svg>
           </div>`;
   modal.innerHTML = /*html*/ `<div id="modalContent">
       <div id="albumSection" class="x">
@@ -116,7 +116,7 @@ function openModal(PlaylistData) {
           </div>
           
         </div>
-        <div><button class="shufflBtn">Shuffle :P</button></div>
+        <div><button class="Btn">Shuffle</button></div>
         <div id="albumSongs" class="albumSongs">
 
       </div>`;
@@ -158,7 +158,7 @@ function openModal(PlaylistData) {
   });
 
   const playlistSection = modal.getElementsByClassName("x");
-  const shufflBtn = modal.getElementsByClassName("shufflBtn");
+  const shufflBtn = modal.getElementsByClassName("Btn");
   shufflBtn[0].addEventListener("click", () =>
     shuffle(PlaylistData, PlaylistData.songs)
   );
@@ -174,7 +174,7 @@ function renderSongs(PlaylistData) {
 
   xbtn = document.createElement("div");
   xbtn.innerHTML = `<div id="close" class="x" style="width: 20px; height: 20px; padding: 10px">
-            X
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512"><!--!Font Awesome Free 6.7.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2025 Fonticons, Inc.--><path fill="#ffffff" d="M342.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L192 210.7 86.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L146.7 256 41.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 301.3 297.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L237.3 256 342.6 150.6z"/></svg>
           </div>`;
   modal.innerHTML = /*html*/ `<div id="modalContent">
       <div id="albumSection" class="x">
@@ -192,7 +192,7 @@ function renderSongs(PlaylistData) {
           </div>
           
         </div>
-        <div><button class="shufflBtn">Shuffle :P</button></div>
+        <div><button class="Btn">Shuffle</button></div>
         <div id="albumSongs" class="albumSongs">
 
       </div>`;
@@ -234,7 +234,7 @@ function renderSongs(PlaylistData) {
   });
 
   const playlistSection = modal.getElementsByClassName("x");
-  const shufflBtn = modal.getElementsByClassName("shufflBtn");
+  const shufflBtn = modal.getElementsByClassName("Btn");
   shufflBtn[0].addEventListener("click", () =>
     shuffle(PlaylistData, PlaylistData.songs)
   );
